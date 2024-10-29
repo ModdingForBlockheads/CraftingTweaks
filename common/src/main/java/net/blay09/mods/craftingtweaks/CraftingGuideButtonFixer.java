5111637795591bd92814cd155cb69e66ba5f128c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-10-29 19:29:00 +0100

    fix: Only move the recipe book button on the vanilla CraftingScreen to not mess with other mods

--- common/src/main/java/net/blay09/mods/craftingtweaks/CraftingGuideButtonFixer.java
+++ common/src/main/java/net/blay09/mods/craftingtweaks/CraftingGuideButtonFixer.java
@@ -1,16 +1,14 @@
 package net.blay09.mods.craftingtweaks;
 
-import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.craftingtweaks.config.CraftingTweaksConfig;
 import net.blay09.mods.craftingtweaks.config.CraftingTweaksConfigData;
 import net.blay09.mods.balm.mixin.AbstractContainerScreenAccessor;
 import net.blay09.mods.balm.mixin.ImageButtonAccessor;
 import net.blay09.mods.balm.mixin.ScreenAccessor;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import net.minecraft.client.gui.screens.inventory.CraftingScreen;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -23,21 +21,10 @@ public class CraftingGuideButtonFixer {
             CraftingTweaksConfigData config = CraftingTweaksConfig.getActive();
             if (config.client.hideVanillaCraftingGuide) {
                 button.visible = false;
-            } else if (!(screen instanceof InventoryScreen)) {
+            } else if (screen instanceof CraftingScreen) {
                 AbstractContainerScreenAccessor accessor = (AbstractContainerScreenAccessor) screen;
                 button.setX(accessor.getLeftPos() + accessor.getImageWidth() - 25);
-
-                // Let's be hacky because fuck this button. Hopefully no one else adds it to their GUIs.
-                if (screen.getClass().getSimpleName().equals("GuiCraftingStation")) {
-                    button.setY(accessor.getTopPos() + 37);
-                } else {
-                    button.setY(accessor.getTopPos() + 5);
-                }
-
-                // Let's be hacky again!
-                if (Balm.isModLoaded("inventorytweaks")) {
-                    button.setX(button.getX() - 15);
-                }
+                button.setY(accessor.getTopPos() + 5);
             }
         }
         return button;
